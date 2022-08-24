fcfc32b1b64c91351438dc3de42e5181ba7fd74c
Mostafa Sakr <msakr3@gmail.com>
2022-08-24 09:17:09 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,10 @@
 class Venue
+    attr_reader :name,
+                :capacity
+
+    def initialize(name, capacity)
+        @name = name                       #venue name
+        @capacity = capacity               #venue capacity
+    end
+
 end
