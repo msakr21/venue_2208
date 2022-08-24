a3b6d33fc1267473fd26f05bae573f982b9b2430
Mostafa Sakr <msakr3@gmail.com>
2022-08-24 09:52:12 -0600

    Finish iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,16 +1,29 @@
 class Venue
+
     attr_reader :name,
                 :capacity,
-                :patrons
+                :patrons,
+                :uppercase_list
 
     def initialize(name, capacity)
         @name = name                       #venue name
         @capacity = capacity               #venue capacity
-        @patrons = []                      #patrons of the venue
+        @patrons = []
+                        #array/list of patrons of the venue
     end
 
-    def add_patron(patron)
-        @patrons << patron
+    def add_patron(patron_name)
+        @patrons << patron_name            #add patron to list of patrons
+    end
+
+
+    def yell_at_patrons
+
+        @patrons.map do |individual|
+           individual.upcase
+        end
+        
+
     end
 
 end
