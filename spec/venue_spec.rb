038a11f84ffd900f30734b3b60077a928fbef3a0
Mostafa Sakr <msakr3@gmail.com>
2022-08-24 10:11:40 -0600

    Change patron names in test to match interaction pattern and start iteration 4

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -65,14 +65,27 @@ describe Venue do
     
     expect(venue.over_capacity).to eq FALSE
 
-    venue.add_patron('Ahmed')
+    venue.add_patron('James')
     expect(venue.over_capacity).to eq FALSE
 
-    venue.add_patron('Erin')
+    venue.add_patron('Cat')
     expect(venue.over_capacity).to eq TRUE
     end
   end
 
+  #Iteration 4
+
+  describe '#over_capacity' do
+    it 'returns true if over capacity' do
+    #skip
+    venue = Venue.new('Bluebird', 4)
+    venue.add_patron('Mike')
+    venue.add_patron('Megan')
+    venue.add_patron('Bob')
+    venue.add_patron('James')
+    venue.add_patron('Cat')
+
+  
  
 
 end
